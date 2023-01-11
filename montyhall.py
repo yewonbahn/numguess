62263d0b93d0e9c13a9e924825f8b10927060169
yewonbahn <banyaewon98@gmail.com>
2023-01-11 10:13:18 +0900

    feat: Add conditinal about change or not change

--- montyhall.py
+++ montyhall.py
@@ -1,8 +1,17 @@
 from random import choice,shuffle
 
 doors = [0,0,1] # sport car =1 
-# 5 번 게임 반복 구현 셋팅
-for i in range(5):
+notchange_cnt = 0
+change_cnt =0
+
+# 100번 게임 반복 구현 셋팅
+for i in range(10000):
     shuffle(doors)
-    choice(doors)
+    answer=choice(doors)
+    if answer==1:
+        notchange_cnt+=1
+    else:
+        change_cnt+=1
+print((change_cnt/10000)*100,"%",(notchange_cnt/10000)*100,"%")
+
     
